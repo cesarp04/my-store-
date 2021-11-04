4f796d23c00da36ab96e4b8cba7d693588127500
Cesar Puentes <caps2695@gmail.com>
2021-11-03 23:23:39 -0500

    separando responsabilidades para las rutas

--- index.js
+++ index.js
@@ -10,23 +10,6 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  res.json({
-    products: [
-      {
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-      },
-      {
-        id: 2,
-        name: 'Product 2',
-        price: 20,
-      },
-    ],
-  });
-});
-
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
