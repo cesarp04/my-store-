6b2cbf72e82c8c24a882e42613f2cc9170dd98d8
Cesar Augusto Puentes Sandoval <42822751+cesarp04@users.noreply.github.com>
2021-11-17 23:42:34 -0500

    delete y update product

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -22,28 +22,27 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
+  const newProduct = service.createProduct(body);
   res.status(201).json({
     message: 'Product created',
-    data: body,
+    data: newProduct,
   });
 });
 
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
+  const updatedProduct = service.updateProduct(id, body);
   res.json({
     message: 'Product Updated',
-    data: body,
-    id,
+    data: updatedProduct,
   });
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    message: 'Product Deleted',
-    id,
-  });
+  const deletedProduct = service.deleteProduct(id);
+  res.json(deletedProduct);
 });
 
 module.exports = router;
--- services/product.service.js
+++ services/product.service.js
@@ -18,12 +18,48 @@ class ProductsService {
     }
   }
 
+  createProduct(product) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      name: product.name,
+      price: product.price,
+      image: product.image,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
   getProducts() {
     return this.products;
   }
 
-  getProductById(id){
-    return this.products.find(item => item.id === id)
+  getProductById(id) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  updateProduct(id, product) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index < 0) {
+      throw new Error('Product not found');
+    }
+    const updatedProduct = this.products[index];
+    this.products[index] = {
+      ...updatedProduct,
+      ...product
+    };
+    return this.products[index];
+  }
+
+  deleteProduct(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index < 0) {
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return {
+      message: 'Product deleted',
+      id,
+    };
   }
 }
 
