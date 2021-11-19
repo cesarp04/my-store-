24b259934fbfbd75086bd9c590e6064a93e8401f
Cesar Augusto Puentes Sandoval <42822751+cesarp04@users.noreply.github.com>
2021-11-18 23:40:28 -0500

    problema de cors

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 
 const routerApi = require('./routes/index.routes');
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3010;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -19,7 +36,7 @@ app.get('/nueva-ruta', (req, res) => {
 
 routerApi(app);
 app.use(logErrors);
-app.use(boomErrorHandler)
+app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.listen(port, () => {
